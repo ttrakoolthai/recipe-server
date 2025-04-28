7646a739a35427dc22b051e5794b6c881121a981
Tommy Trakoolthai <77364631+ttrakoolthai@users.noreply.github.com>
2025-04-27 22:06:22 -0700

    Created new Rust project

---
+++ target/debug/deps/recipe_server-b9a1083eac358888.d
@@ -0,0 +1,5 @@
+/Users/trakoolthai/Desktop/recipe-server/target/debug/deps/recipe_server-b9a1083eac358888: src/main.rs
+
+/Users/trakoolthai/Desktop/recipe-server/target/debug/deps/recipe_server-b9a1083eac358888.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/recipe-server.d
@@ -0,0 +1,1 @@
+/Users/trakoolthai/Desktop/recipe-server/target/debug/recipe-server: /Users/trakoolthai/Desktop/recipe-server/src/main.rs
