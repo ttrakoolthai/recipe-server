4c5a562ae6209f422e674032ef56513825aee257
Tommy Trakoolthai <77364631+ttrakoolthai@users.noreply.github.com>
2025-04-28 16:49:39 -0700

    Set up for multi-recipe reads

--- src/joke.rs
+++ src/joke.rs
@@ -1,9 +1,14 @@
-pub struct Joke {
-    pub whos_there: &'static str,
-    pub answer: &'static str,
-}
+use serde::Deserialize;
 
-pub const THE_JOKE: Joke = Joke {
-    whos_there: "Boo",
-    answer: "You don't have to cry about it!",
-};
+ #[derive(Deserialize)]
+ pub struct Joke {
+     pub whos_there: &'static str,
+     pub answer: &'static str,
+     pub answer_who: &'static str,
+ }
+
+ pub const THE_JOKE: Joke = Joke {
+     whos_there: "Boo",
+     answer: "You don't have to cry about it!",
+     answer_who: "You don't have to cry about it!",
+ };
